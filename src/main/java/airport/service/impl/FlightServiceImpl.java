4ad0e57a7706f6b0b23384cbf572fd40955d3314
BohdanShchehliuk <shbp87@gmail.com>
2022-06-25 12:39:49 +0300

    Repository

--- src/main/java/airport/service/impl/FlightServiceImpl.java
+++ src/main/java/airport/service/impl/FlightServiceImpl.java
@@ -2,11 +2,9 @@ package airport.service.impl;
 
 import airport.dto.FlightDto;
 import airport.entity.Flight;
-import org.hibernate.query.NativeQuery;
-import org.hibernate.query.internal.NativeQueryImpl;
+import org.hibernate.Session;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
 import airport.repository.FlightRepository;
 
@@ -17,10 +15,10 @@ import java.util.stream.Collectors;
 
 @Service
 public class FlightServiceImpl implements airport.service.FlightService {
-
-    ModelMapper modelMapper = new ModelMapper();
+    private Session session;
+    private ModelMapper modelMapper = new ModelMapper();
     @Autowired
-    FlightRepository flightRepository;
+    private FlightRepository flightRepository;
 
     public Flight addFlight(Flight flight) {
         return flightRepository.save(flight);
@@ -33,7 +31,7 @@ public class FlightServiceImpl implements airport.service.FlightService {
 
     @Override
     public List<FlightDto> getAllFomDataAtoDataB(LocalDate startData, LocalDate finishData) {
-                    return getAll().stream().
+        return getAll().stream().
                 filter(flight -> flight.getTime().toLocalDate().isAfter(startData) &&
                         flight.getTime().toLocalDate().isBefore(finishData))
                 .map(flight -> modelMapper
@@ -47,3 +45,9 @@ public class FlightServiceImpl implements airport.service.FlightService {
     }
 }
 
+//    Date dateOne = Date.from(startData.atStartOfDay(ZoneId.systemDefault()).toInstant());
+//    Date dateTwo = Date.from(startData.atStartOfDay(ZoneId.systemDefault()).toInstant());
+//
+//    NativeQuery query = (NativeQuery) session.createQuery("\"from Flight f where f.time > ?"+dateOne+" and " +
+//            "f.time < ?"+dateTwo+"\"");
+//    List<FlightDto> list = query.list();
