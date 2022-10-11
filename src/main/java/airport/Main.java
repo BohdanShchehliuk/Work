ff9311c0f7d252c459b4062edfda71c724d09ddd
BohdanShchehliuk <shbp87@gmail.com>
2022-10-11 12:28:47 +0300

    One

--- src/main/java/airport/Main.java
+++ src/main/java/airport/Main.java
@@ -6,22 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
-       public static void main(String[] args) throws UserNotFoundException {
+       public static void main(String[] args)  {
          SpringApplication.run(Main.class, args);
-//           FlightRepository flightRepository =  context.getBean(FlightRepository.class);
-//            System.out.println(flightRepository.findAll());
 
     }
 }
-
-
-
-
-
-//    @Bean
-//    public CommandLineRunner run(ApplicationContext appContext) {
-//        return args -> {
-//            String[] beans = appContext.getBeanDefinitionNames();
-//            Arrays.stream(beans).sorted().forEach(System.out::println);
-//        };
-//    }
