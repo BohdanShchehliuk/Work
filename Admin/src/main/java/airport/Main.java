bd3eb64bad15111b674a4f4108a8eff4e6b8707e
BohdanShchehliuk <shbp87@gmail.com>
2022-10-28 15:54:50 +0300

    Eureka_almost_done

--- Admin/src/main/java/airport/Main.java
+++ Admin/src/main/java/airport/Main.java
@@ -3,10 +3,13 @@ package airport;
 import airport.exception.UserNotFoundException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Main {
-    public static void main(String[] args) throws UserNotFoundException {
+      public static void main(String[] args) throws UserNotFoundException {
         SpringApplication.run(Main.class, args);
     }
 }
